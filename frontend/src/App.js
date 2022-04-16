84e47a34e1ab384ea7adcf6d153fb22c7e834d47
Devanshu Ruhela <devanshuruhela2.0@gmail.com>
2022-04-16 13:57:23 +0530

    Global navigation and Selecting color scheme

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,16 @@
-import './App.css';
-import {BrowserRouter, Routes , Route}  from 'react-router-dom'
-import Home from './pages/Home/home'
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/home";
+import Navigation from "./Components/Shared/Navigation/navigation.component";
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path='/' exact element={<Home/>}>
-      </Route>
-    </Routes>
+      <Navigation />
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+      </Routes>
     </BrowserRouter>
-    )
+  );
 }
 
 export default App;
---
+++ frontend/src/Components/Shared/Navigation/navigation.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './navigation.module.css'
+const Navigation = () => {
+  return (
+    <div>Navigation</div>
+  )
+}
+
+export default Navigation;
