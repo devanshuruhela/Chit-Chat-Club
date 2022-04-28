a21d6a58ad9704e3fb41692fe70f31213551dd0a
Devanshu Ruhela <devanshuruhela2.0@gmail.com>
2022-04-28 17:51:37 +0530

    Rooms page header and structure

--- frontend/src/pages/Rooms/rooms.jsx
+++ frontend/src/pages/Rooms/rooms.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
-
+import './rooms.styles.css'
+import searchicon from '../../images/search-icon.png'
+import addroom from '../../images/add-room-icon.png'
 const Rooms = () => {
   return (
-    <div>Rooms</div>
+    <>
+    <div className="container">
+      <div className="roomsHeader">
+        <div className="left">
+          <span className='heading'> All voice Rooms</span>
+          <div className="searchbox">
+            <img src={searchicon} alt="search" />
+            <input type="text" className='searchinput' />
+          </div>
+        </div>
+        <div className="right">
+          <button className='Newroombtn'>
+            <img src={addroom} alt="addroom" />
+            <span>Start a Room</span>
+          </button>
+        </div>
+      </div>
+    </div>
+    </>
   )
 }
 
