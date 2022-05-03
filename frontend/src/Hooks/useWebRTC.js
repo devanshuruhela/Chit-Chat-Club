c3bdbaefd43d6f0fa59a812ed2715d2f63907f40
Devanshu Ruhela <devanshuruhela2.0@gmail.com>
2022-05-03 17:24:11 +0530

    Setting up mediastream for owner of room as speaker

--- frontend/src/Hooks/useWebRTC.js
+++ frontend/src/Hooks/useWebRTC.js
@@ -1,23 +1,50 @@
+import { useCallback, useEffect, useRef } from "react";
 import { useStateWithCallback } from "./useStateWithCallback";
 
-const users = [
-  {
-    id: 1,
-    name: "Name1",
-  },
-  {
-    id: 2,
-    name: "Name 2",
-  },
-];
 export const useWebRCT= (roomId ,user) =>
 {
-  const [clients, setClients] = useStateWithCallback(users);
+  const [clients, setClients] = useStateWithCallback([]);
+  const audioElements = useRef({});
+  const connections =useRef({});
+  const localMediaStream = useRef(null)
 
-  // setClients((prev)=> {} , (state)=>
-  // {
+  const provideRef=(instance, userId) =>
+    {
+      audioElements.current[userId] = instance
+    }
 
-  // })
+  const addNewClients = useCallback((newClient , cb)=>
+  {
+    const lookingFor = clients.find((client) => client.id === newClient.id);
+    if(lookingFor === undefined)
+    {
+      setClients((existingClients)=> [...existingClients , newClient] , cb)
+    }
+  }, [clients , setClients])
 
-  return {clients}
+  useEffect(()=>
+  {
+    const startCapture = async () =>
+    {
+      localMediaStream.current =  await navigator.mediaDevices.getUserMedia(
+        {
+          audio:true,
+        }
+      )
+    }
+
+    startCapture().then(()=>
+    {
+      addNewClients(user , ()=> {
+        const localaudioelt = audioElements.current[user.id];
+        if(localaudioelt)
+        {
+          localaudioelt.volume = 0;
+          localaudioelt.srcObject = localMediaStream.current
+        }
+      })
+    });
+  },[])
+  
+  return {clients ,provideRef}
 }
--- frontend/src/pages/Roompage/roompage.jsx
+++ frontend/src/pages/Roompage/roompage.jsx
@@ -1,12 +1,12 @@
-import React, { useState } from 'react'
 import { useSelector } from 'react-redux'
 import { useParams } from 'react-router-dom'
 import { useWebRCT } from '../../Hooks/useWebRTC'
 import './roompage.styles.css'
 const RoomPage = () => {
   const {id : roomId} =useParams();
-  const {user} = useSelector(state=> state.auth.user)
-  const {clients} = useWebRCT(roomId , user)
+  const user = useSelector(state=> state.auth.user)
+  const {clients , provideRef} = useWebRCT(roomId , user)
+
   return (
     <div>
       <h1>All connected Clients</h1>
@@ -14,7 +14,7 @@ const RoomPage = () => {
         clients.map((client)=>
         {
           return (<div key={client.id}>
-            <audio controls autoPlay></audio>
+            <audio ref={(instance) => provideRef(instance , client.id)} controls autoPlay></audio>
             <h4>{client.name}</h4>
           </div>)
         })
